a77b6cfe62009b8aed9751892dedd248bba97093
PermissionDog <455662503@qq.com>
2022-09-27 12:54:56 +0800

    build: 区分开发模式和构建模式,分别设置代理

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,31 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        vuetify({ autoImport: true }),
-    ],
-    server:{
-        proxy:{
-            '/api':{
-                target:'http://localhost:5277/api/',
-                changeOrigin:true,
-                secure:false,
-                rewrite:(path)=>
-                    path.replace("^/api",'')
+export default defineConfig(({command, mode}) => {
+    if (command == 'serve') {
+        return {
+            plugins: [
+                vue(),
+                vuetify({ autoImport: true }),
+            ],
+            server:{
+                proxy:{
+                    '/api':{
+                        target:'http://localhost:5277/api/',
+                        changeOrigin:true,
+                        secure:false,
+                        rewrite:(path)=>
+                            path.replace("^/api",'')
+                    }
+                }
             }
+        }
+    } else {
+        return {
+            plugins: [
+                vue(),
+                vuetify({ autoImport: true }),
+            ]
         }
     }
 })
